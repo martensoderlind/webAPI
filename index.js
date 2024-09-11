79374dd2cfbc8057b7d6ed565c320e476231d4e7
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-09-11 12:52:44 +0200

    added server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("started");
+
+const server = http.createServer((req, res) =>{
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/plain');
+    res.end('Hello fellow developer!');
+});
