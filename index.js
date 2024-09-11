4c64781c6ef78cc91516460cae4eb103aee6c086
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-09-11 14:36:31 +0200

    changed name of func fileName to fileNameOfUrl

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("started");
 const http = require('http');
 const fs=require('fs')
 
-const fileName = url =>{
+const fileNameOfUrl = url =>{
     let fileName = '';
     if(req.url.split('/')[1]===''){
         fileName="index.html"
@@ -15,9 +15,13 @@ const fileName = url =>{
 const server = http.createServer((req, res) =>{
     console.log(`The URL for the request was '${req.url}'`);
     console.log(`Method: '${req.method}'`);
+
+    const fileName = fileNameOfUrl (req.url);
+    const Content = fs.readFileSync(`./static/${fileName}`,'utf-8');
+
     res.statusCode=200;
     res.setHeader('Content-Type','text/html');
-    res.end(fs.readFileSync('./static/index.html','utf-8'));
+    res.end(Content);
 });
 
 const hostname = 'localhost';
