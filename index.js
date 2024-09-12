5f18c6434b9fda6b78e0ea45bb6731f71e127e3e
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-09-12 18:41:33 +0200

    post requst for new dev

--- index.js
+++ index.js
@@ -13,24 +13,38 @@ const db =[
 },
 ];
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res
       .status(201)
       .setHeader(`location`,`/api/developer/1`)
       .json(db[0])
-  });
+});
 
-  app.get('/api/developers',(req, res)=>{
-    res.json(db);
-  });
+app.get('/api/developers',(req, res)=>{
+  res.json(db);
+});
 
-  app.get('/api/developers/:id',(req, res)=>{
-    const dev = db.find((dev)=>dev.id==req.params.id);
-    return dev ? 
-      res.json(dev):
-      res.status(404).end();
-  });
+app.get('/api/developers/:id',(req, res)=>{
+  const dev = db.find((dev)=>dev.id==req.params.id);
+  return dev ? 
+    res.json(dev):
+    res.status(404).end();
+});
+
+app.post('api/developers/',(req,res)=>{
+  const newDev={
+    id: db.length+1,
+    name: req.body.name,
+    email: req.body.email
+  };
+  db.push(newDev);
+  res
+    .status(201)
+    .setHeader(`location`,`/api/developer/${newDev.id}`)
+    .json(newDev);
+});
 
 const port = 3000;
 app.listen(port, ()=>{
